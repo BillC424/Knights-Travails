6fb12bb8b28bd2f1159e0cf9c615c3dd26100c77
BillC424 <wcurran44@gmail.com>
2023-03-24 17:39:13 -0400

    Added board_corner_adjacent_square method to return the array of squares adjacent to each corner square

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,13 +1,16 @@
 class BoardSquare
+  attr_accessor :board_position, :adjacent_squares, :game_piece
+
   def initialize(row,column)
     @board_position = [row,column]
+    @adjacent_squares = []
     @game_piece = nil
   end
 end
 
 class GameBoard
   def initialize()
-    @board = build_board()
+    @board = build_board#determine_adjacent_squares
     @knight = Knight.new
   end
 
@@ -18,22 +21,37 @@ class GameBoard
        board.push(BoardSquare.new(n,i))
       end
     end
+    determine_adjacent_squares(board)
+  end
+
+  def determine_adjacent_squares(board)
+    board.each do |board_square|
+      p board_square.board_position
+      board_square.adjacent_squares = board_corner_adjacent_square(board_square.board_position) if board_square.board_position == [0, 0] || [7, 0] || [0, 7] || [7, 7]
+      p board_square
+    end
     board
   end
 
-  def knight_moves(starting_square, destination_square)
-  
+  def board_corner_adjacent_square(board_square)
+   #Left corners
+   return [0, 1], [1, 0], [1, 1] if board_square == [0, 0]
+   return [7, 1], [6, 0], [7, 1] if board_square == [7, 0]
+   #Right corners
+   return [0, 6], [1, 6], [1, 7] if board_square == [0, 7]
+   return [7, 6], [6, 6], [6, 7] if board_square == [7, 7] 
   end
 
 end
 
 class Knight
   def move()
-    #move two spaces and then one space to left or right
+    #move two spaces and then one space to left, right, up, or down
+  end
+
+  def knight_moves(starting_square, destination_square)
+    #shows the shortest possible way to get from one square to another by outputting all squares the knight will stop on along the way.
   end
 end
 
 board = GameBoard.new
-
-p board
-
