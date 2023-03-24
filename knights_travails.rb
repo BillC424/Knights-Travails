e7513765ed0b6fd70e1e48fabfa50f77777c4e8d
BillC424 <wcurran44@gmail.com>
2023-03-24 18:05:58 -0400

    Moved board_corner_adjacent_squares method call inside of first_and_last_rows_adjacent_squares method to reduce lines of code in determine_adjacent_squares method

--- knights_travails.rb
+++ knights_travails.rb
@@ -26,20 +26,23 @@ class GameBoard
 
   def determine_adjacent_squares(board)
     board.each do |board_square|
-      p board_square.board_position
-      board_square.adjacent_squares = board_corner_adjacent_square(board_square.board_position) if board_square.board_position == [0, 0] || [7, 0] || [0, 7] || [7, 7]
-      p board_square
+       board_square.adjacent_squares = first_and_last_rows_adjacent_squares(board_square.board_position) if board_square.board_position[0] == 0 || board_square.board_position[0] == 7
     end
     board
   end
 
-  def board_corner_adjacent_square(board_square)
+  def board_corner_adjacent_squares(board_square)
    #Left corners
    return [0, 1], [1, 0], [1, 1] if board_square == [0, 0]
    return [7, 1], [6, 0], [7, 1] if board_square == [7, 0]
    #Right corners
    return [0, 6], [1, 6], [1, 7] if board_square == [0, 7]
    return [7, 6], [6, 6], [6, 7] if board_square == [7, 7] 
+  end
+
+  def first_and_last_rows_adjacent_squares(board_square)
+    return board_corner_adjacent_squares(board_square) if board_square == [0, 0] || board_square == [7, 0] || board_square == [0, 7] || board_square == [7, 7]
+
   end
 
 end
@@ -55,3 +58,5 @@ class Knight
 end
 
 board = GameBoard.new
+
+p board
