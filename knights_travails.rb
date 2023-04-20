bbbbabc501267c77822706bbb49a94ddb4db42ca
BillC424 <wcurran44@gmail.com>
2023-04-19 23:29:03 -0400

    Removed BoardSquare and GameBoard classes, which were not needed to meet the requirements of the project.

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,94 +1,5 @@
 # frozen_string_literal: true
 
-class BoardSquare
-  attr_accessor :board_position, :adjacent_squares, :game_piece
-
-  def initialize(row, column)
-    @board_position = [row, column]
-    @adjacent_squares = []
-    @game_piece = nil
-  end
-end
-
-class GameBoard
-  attr_accessor :board, :knight
-
-  def initialize
-    @board = build_board
-    @knight = Knight.new
-  end
-
-  def build_board
-    board = []
-    8.times do |n|
-    end
-    determine_adjacent_squares(board)
-  end
-
-  def determine_adjacent_squares(board)
-    board.each do |board_square|
-      board_square.adjacent_squares = if board_square.board_position[0].zero? || board_square.board_position[0] == 7
-                                        first_and_last_rows_adjacent_squares(board_square.board_position)
-                                      else
-                                        middle_rows_adjacent_squares(board_square.board_position)
-                                      end
-    end
-    board
-  end
-
-  def adjacent_square_array_builder(board_square, adjacent_square_positions, adjacent_squares)
-    adjacent_square_positions.each do |square|
-      adjacent_square = [board_square[0] + square[0], board_square[1] + square[1]]
-      adjacent_squares << adjacent_square
-    end
-  end
-
-  def board_corner_adjacent_squares(board_square)
-    # Left corners
-    return [0, 1], [1, 0], [1, 1] if board_square == [0, 0]
-    return [7, 1], [6, 0], [6, 1] if board_square == [7, 0]
-    # Right corners
-    return [0, 6], [1, 6], [1, 7] if board_square == [0, 7]
-    return [7, 6], [6, 6], [6, 7] if board_square == [7, 7]
-  end
-
-  def first_and_last_rows_adjacent_squares(board_square, adjacent_squares = [])
-    return board_corner_adjacent_squares(board_square) if [[0, 0], [7, 0], [0, 7], [7, 7]].include?(board_square)
-
-    adjacent_square_positions = [[0, -1], [+1, -1], [+1, 0], [+1, +1], [0, +1]]
-    adjacent_square_array_builder(board_square, adjacent_square_positions, adjacent_squares)
-    adjacent_squares
-  end
-
-  def middle_rows_edge_squares(board_square)
-    return middle_rows_edge_first_row(board_square) if (board_square[1]).zero?
-    return middle_rows_edge_last_row(board_square) if board_square[1] == 7
-  end
-
-  def middle_rows_edge_first_row(board_square, adjacent_squares = [])
-    adjacent_square_positions = [[-1, 0], [-1, +1], [0, +1], [+1, +1], [+1, 0]]
-    adjacent_square_array_builder(board_square, adjacent_square_positions, adjacent_squares)
-    adjacent_squares
-  end
-
-  def middle_rows_edge_last_row(board_square, adjacent_squares = [])
-    adjacent_square_positions = [[-1, -1], [0, -1], [+1, -1], [+1, 0], [-1, 0]]
-    adjacent_square_array_builder(board_square, adjacent_square_positions, adjacent_squares)
-    adjacent_squares
-  end
-
-  def middle_rows_adjacent_squares(board_square, adjacent_squares = [])
-    return middle_rows_edge_squares(board_square) if (board_square[1]).zero? || board_square[1] == 7
-
-    adjacent_square_positions = [[-1, -1], [0, -1], [1, -1],
-                                 [1, 0],
-                                 [1, 1], [0, 1], [-1, 1],
-                                 [-1, 0]]
-    adjacent_square_array_builder(board_square, adjacent_square_positions, adjacent_squares)
-    adjacent_squares
-  end
-end
-
 class Node
   attr_accessor :previous_square
   attr_reader :board_position, :subsequent_position
@@ -206,6 +117,6 @@ class Knight
   end
 end
 
-board = GameBoard.new
+knight = Knight.new
 
-board.knight.knight_moves([3, 3], [2, 3])
+knight.knight_moves([3, 3], [2, 3])
